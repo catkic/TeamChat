ae23f5623235d1a9398f08a3ea612d75ece8a1b9
Catkic <476646484@qq.com>
2021-07-28 13:52:46 +0000

    update log4cxx url

--- server/src/make_log4cxx.sh
+++ server/src/make_log4cxx.sh
@@ -4,6 +4,7 @@
 
 LOG4CXX=apache-log4cxx-0.10.0
 LOG4CXX_PATH=http://mirror.bit.edu.cn/apache/logging/log4cxx/0.10.0/$LOG4CXX.tar.gz
+LOG4CXX_PATH=https://archive.apache.org/dist/logging/log4cxx/0.10.0/$LOG4CXX.tar.gz
 CUR_DIR=
 download() {
     if [ -f "$1" ]; then
